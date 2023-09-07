7e153355dd2c6c9b2573e5ef9c87caace5b09f2c
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-07 17:53:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showOverlay(epubPath1, epubPath2) {
+function showOverlay(epubPath1, epubPath2, epubPath3) {
     // Set the download link href to the first ePub file path
     document.getElementById('downloadLink').href = epubPath1;
 
@@ -9,6 +9,15 @@ function showOverlay(epubPath1, epubPath2) {
     } else {
         // Hide the second download link if no second ePub path is provided
         document.getElementById('downloadLink2').style.display = 'none';
+    }
+
+    // Set the third download link href for the "Astro" image
+    if (epubPath3) {
+        document.getElementById('downloadLink3').href = epubPath3;
+        document.getElementById('downloadLink3').style.display = 'inline-block';
+    } else {
+        // Hide the third download link if no third ePub path is provided
+        document.getElementById('downloadLink3').style.display = 'none';
     }
 
     // Show the overlay
