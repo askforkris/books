edeb3d721daf34f8a50ef2429e93439f7dd5eee3
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-09 17:23:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function showOverlay(epubPath1, epubPath2, epubPath3) {
-    // Set the download link href to the first ePub file path
-    document.getElementById('downloadLink').href = epubPath1;
+function showOverlay() {
+    var epubPath = 'Ciany.epub';
 
-    // Set the second download link href for the "Astro" image
-    if (epubPath2) {
-        document.getElementById('downloadLink2').href = epubPath2;
-        document.getElementById('downloadLink2').style.display = 'inline-block';
-    } else {
-        // Hide the second download link if no second ePub path is provided
-        document.getElementById('downloadLink2').style.display = 'none';
-    }
-
-    // Set the third download link href for the "Astro" image
-    if (epubPath3) {
-        document.getElementById('downloadLink3').href = epubPath3;
-        document.getElementById('downloadLink3').style.display = 'inline-block';
-    } else {
-        // Hide the third download link if no third ePub path is provided
-        document.getElementById('downloadLink3').style.display = 'none';
-    }
+    // Set the download link href to the ePub file path
+    document.getElementById('downloadLink').href = epubPath;
 
     // Show the overlay
     document.getElementById('imageOverlay').style.display = 'block';
