4cd72abd22d9ce88da54b3e44f99e35eae91a448
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-09 17:32:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function showOverlay(epubPath) {
-    // Set the download link href to the ePub file path
-    document.getElementById('downloadLink').href = epubPath;
+   function showOverlay(epubPath) {
+        // Set the download link href to the ePub file path
+        document.getElementById('downloadLink').href = epubPath;
 
-    // Show the overlay
-    document.getElementById('imageOverlay').style.display = 'block';
-}
+        // Show the overlay
+        document.getElementById('imageOverlay').style.display = 'block';
+    }
 
-function hideOverlay() {
-    // Hide the overlay
-    document.getElementById('imageOverlay').style.display = 'none';
-}
+    function hideOverlay() {
+        // Hide the overlay
+        document.getElementById('imageOverlay').style.display = 'none';
+    }
+</script>
+
+</body>
+</html>
