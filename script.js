6301181ef04c6f69adf377f01c452ee9ac1cc3cf
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-09 20:32:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function showOverlay() {
-    var epubPath = 'Ciany.epub';
+  function showOverlay(epubPath) {
+        // Set the download link href to the ePub file path
+        document.getElementById('downloadLink').href = epubPath;
 
-    // Set the download link href to the ePub file path
-    document.getElementById('downloadLink').href = epubPath;
+        // Show the overlay
+        document.getElementById('imageOverlay').style.display = 'block';
+    }
 
-    // Show the overlay
-    document.getElementById('imageOverlay').style.display = 'block';
-}
-
+    function hideOverlay() {
+        // Hide the overlay
+        document.getElementById('imageOverlay').style.display = 'none';
