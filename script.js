4d0c4adbdb6b4b94f0cb3facd816afe9318d987c
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-16 21:33:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-  function showOverlay(epubPath) {
-        // Set the download link href to the ePub file path
-        document.getElementById('downloadLink').href = epubPath;
-
-        // Show the overlay
-        document.getElementById('imageOverlay').style.display = 'block';
+const bios = [
+    {
+        text: "Bio for Astro: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam velit. Nulla ut magna lobortis, egestas erat quis, aliquet est. Sed vel massa a felis imperdiet ultricies. Duis vel auctor arcu.",
+        epubPath: '3001.epub'
+    },
+    {
+        text: "Bio for Ciany: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam velit. Nulla ut magna lobortis, egestas erat quis, aliquet est. Sed vel massa a felis imperdiet ultricies. Duis vel auctor arcu.",
+        epubPath: 'Ciany.epub'
+    },
+    {
+        text: "Bio for Image 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam velit. Nulla ut magna lobortis, egestas erat quis, aliquet est. Sed vel massa a felis imperdiet ultricies. Duis vel auctor arcu.",
+        epubPath: 'image3.epub'
+    },
+    {
+        text: "Bio for Image 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam velit. Nulla ut magna lobortis, egestas erat quis, aliquet est. Sed vel massa a felis imperdiet ultricies. Duis vel auctor arcu.",
+        epubPath: 'image4.epub'
     }
+];
 
-    function hideOverlay() {
-        // Hide the overlay
-        document.getElementById('imageOverlay').style.display = 'none';
+function showOverlay(index) {
+    // Set the bio text and download link href based on the selected index
+    document.getElementById('bioText').textContent = bios[index].text;
+    document.getElementById('downloadLink').href = bios[index].epubPath;
+
+    // Show the overlay
+    document.getElementById('imageOverlay').style.display = 'block';
+}
+
+function hideOverlay() {
+    // Hide the overlay
+    document.getElementById('imageOverlay').style.display = 'none';
+}
+
